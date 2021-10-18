7e569fc548f6c0df4444453664572881d210c884
Michael Roddy <88041719+michaeljroddy@users.noreply.github.com>
2021-10-18 15:06:18 +0100

    small change

--- notification_service.py
+++ notification_service.py
@@ -21,6 +21,7 @@ ENVIRONMENT = os.environ["GENESYS_CLOUD_ENVIRONMENT"] # eg. mypurecloud.com or m
 
 def find_queue_id(queue_name, routing_api_instance):
     try:
+        # find queue id by filtering all queues by queue name
         response = routing_api_instance.get_routing_queues(name=queue_name)
         if response.entities == []:
             return
@@ -33,11 +34,11 @@ def find_queue_id(queue_name, routing_api_instance):
     except ApiException as e:
         print("Exception when calling RoutingApi->get_routing_queues: %s\n" % e)
     
-  
+
 def create_notifications_channel(notifications_api_instance):
     try:
         print("\nCreating notifications channel..")
-        # Create a new channel
+        # Create the channel
         response = notifications_api_instance.post_notifications_channels()
         return response
     except ApiException as e:
@@ -50,8 +51,8 @@ def subscribe_to_topic(queue_id, channel_id, notifications_api_instance):
         {"id": f"v2.routing.queues.{queue_id}.users"}
     ]
     try:
-        # Replace the current list of subscriptions with a new list.
         print(f"\nSubscribing to v2.routing.queues.{queue_id}.users")
+         # Replace the current list of subscriptions with a new list.
         notifications_api_instance.put_notifications_channel_subscriptions(channel_id, body)
     except ApiException as e:
         print("\nException when calling NotificationsApi->post_notifications_channel_subscriptions: %s\n" % e)
@@ -59,32 +60,38 @@ def subscribe_to_topic(queue_id, channel_id, notifications_api_instance):
 
 async def listen(uri, queue_name):
     print("\nOpening web socket connection to notifications channel..")
+    # open the websocket connection
     async with websockets.connect(uri) as websocket:
         print(f"\nConnected! You can now listen to notification events from {queue_name}")
         loop = asyncio.get_event_loop()
         loop.add_signal_handler(signal.SIGINT, loop.create_task, websocket.close())
         async for response in websocket:
+            # format messages
             json_object = json.loads(response)
             json_formatted_str = json.dumps(json_object, indent=4)
             print("\n" + json_formatted_str)
 
 
 def main():
-    # Configure OAuth2 access token for authorization: PureCloud OAuth
-    api_client = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(os.environ['GENESYS_CLOUD_CLIENT_ID'], os.environ['GENESYS_CLOUD_CLIENT_SECRET'])
+    # Authenticate
+    api_client = PureCloudPlatformClientV2.api_client.ApiClient().get_client_credentials_token(os.environ["GENESYS_CLOUD_CLIENT_ID"], os.environ["GENESYS_CLOUD_CLIENT_SECRET"])
     # create an instance of the API class
     notifications_api_instance = PureCloudPlatformClientV2.NotificationsApi(api_client)
     routing_api_instance = PureCloudPlatformClientV2.RoutingApi(api_client)
     queue_name = input("\nEnter the name of the queue you want to listen to: ")
+    # find the queue id for the queue you want to listen to
     queue_id = find_queue_id(queue_name, routing_api_instance)
     if queue_id == None:
         print("\nQUEUE NOT FOUND..")
         sys.exit(0)
+    # 1. create the notifications channel
     response = create_notifications_channel(notifications_api_instance)
-    # extract connect uri and channel id from response
+    # 1.1 extract connect uri and channel id from the response
     uri = response.connect_uri
     channel_id = response.id
+    # 2. subscribe to a topic 
     subscribe_to_topic(queue_id, channel_id, notifications_api_instance)
+    # 3. open a web socket connection to the notifications channel
     asyncio.get_event_loop().run_until_complete(listen(uri, queue_name))
 
 
